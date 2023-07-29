ae5459646f6eba33f7b7a8242bda04d158bf5bdb
Devashish jadhav <jadhavom24@gmail.com>
2023-07-29 23:26:29 +0530

    added hero section

--- script.js
+++ script.js
@@ -1,6 +1,13 @@
 const mobileBtn = document.querySelector("#mobileBtn")
 const navMobile = document.querySelector('.navMobile')
+const nav = document.querySelector('nav') 
 
 mobileBtn.addEventListener('click', () => {
   navMobile.classList.toggle('show')
+})
+
+window.addEventListener('scroll', () => {
+  let value = window.scrollY
+  
+  nav.style.top = value * 0.5 + "px"
 })
